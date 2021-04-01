bf9d8a50ce634490ee70f207ccacf8bcdd4c2e53
AissiouAbdallahDevFs <aissiou.abdallah@gmail.com>
2021-04-01 11:34:39 +0200

    MiseEnPLaceDuCrudUser

---
+++ src/Form/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('isVerified')
+            ->add('lastname')
+            ->add('firstname')
+            ->add('city')
+            ->add('address')
+            ->add('postcode')
+            ->add('phone')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
