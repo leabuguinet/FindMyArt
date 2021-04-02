2d2b8baf65ade06ce649ef8697ea7c01fc5efd3f
Léa Buguinet <lea.buguinet@gmail.com>
2021-04-02 16:34:20 +0200

    EnvoiMail

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +14,7 @@ class ContactType extends AbstractType
     {
         $builder
             ->add('field_name')
-            ->add('email', Email::class)
+            ->add('email', EmailType::class)
             ->add('phone')
             ->add('message', TextareaType::class)
         ;
