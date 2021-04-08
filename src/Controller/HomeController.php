e60ec1fea0a2c2b83bfd307c24591f309971f68c
AissiouAbdallahDevFs <aissiou.abdallah@gmail.com>
2021-04-08 13:47:05 +0200

    MiseEnPlaceCollection

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PieceRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(): Response
+    public function index(PieceRepository $pieceRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'pieces' => $pieceRepository->findAll(),
         ]);
     }
+    
 
       /**
      * @Route("/", name="homepage", methods={"GET","POST"})
@@ -27,4 +30,3 @@ class HomeController extends AbstractController
     
    
 }
-   
