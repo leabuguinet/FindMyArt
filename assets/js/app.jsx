9b8d19d863c0a6b8896b13d60c8b41a362d80b1e
AissiouAbdallahDevFs <aissiou.abdallah@gmail.com>
2021-04-08 17:17:39 +0200

    ComitTwigCssAppJsx

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -9,21 +9,6 @@ import {
 export default class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-      <Route path= "/concept"><div>haha</div></Route>
-      <Route><div>click</div></Route>
-      <div class="container collection">
-        <div class="row">
-          <div class="col-sm">
-            <i class="fas fa-filter"></i>
-          </div>
-          <div class="col-sm">
-            <p>for of</p>
-            <h2>lol</h2>
-          </div>
-        </div>
-      </div>
-      </BrowserRouter>
     )
   }
 }
