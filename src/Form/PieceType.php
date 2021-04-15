862978c843dea5666d67fd58cf1a715935a8465e
Nathalie <nathaliethisse@gmail.com>
2021-04-15 12:36:20 +0200

    CreateNewRenting- fixed

--- src/Form/PieceType.php
+++ src/Form/PieceType.php
@@ -13,7 +13,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 use App\Entity\Owner;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
