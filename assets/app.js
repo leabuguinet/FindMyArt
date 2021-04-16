8b45455fa66dbe91adda49ec0befa89ccfd2bfbe
Blt <julien.bellt.80@gmail.com>
2021-04-16 09:27:00 +0200

    CSSConcept3.0

--- assets/app.js
+++ assets/app.js
@@ -19,4 +19,42 @@ if(RouteElement){
     ReactDOM.render(<App/>, document.getElementById('root'));
 }
 
+//Fonction permettant d'afficher les infos Particulier/Entreprise
 
+function showInfo1() {
+
+        let div = document.getElementById("info1");
+
+        if (div.style.opacity == "0" || div.style.opacity == "") {
+                 div.style.opacity = "1";
+        } 
+        else {
+                div.style.opacity = "0";
+        }
+
+};
+
+let button = document.getElementById('button1');
+
+button.addEventListener('click', showInfo1);
+
+
+function showInfo2() {
+
+        let div2 = document.getElementById("info2");
+
+        if (div2.style.opacity == "0" || div2.style.opacity == "") {
+                 div2.style.opacity = "1";
+        } 
+        else {
+                 div2.style.opacity = "0";
+        }
+
+        
+};
+
+
+
+let button2 = document.getElementById('button2');
+button2.addEventListener('click', showInfo2);
+   
