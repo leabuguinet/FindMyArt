b5a6018c9d6fd8a6683dbf6547908a8d0e41905e
Nathalie <nathaliethisse@gmail.com>
2021-04-17 07:53:20 +0200

    Début Page Show Renting(depuis espace perso)

---
+++ src/Controller/ShowRentingController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\Renting;
+use App\Form\RentingType;
+use App\Repository\RentingRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShowRentingController extends AbstractController
+{
+    #[Route('/show/renting/{id}', name: 'show_renting', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager, $id): Response
+    {
+        $user = $this->getUser();
+        $renting = $entityManager->getRepository(Renting::class)->find($id);
+        return $this->render('show_renting/index.html.twig', [
+            'user' => $user,            
+            'rentings' => $user->getRentings(),
+            'renting' => $renting,
+        ]);
+    }
+}
