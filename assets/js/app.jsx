400fed211bf417473a83f301c222d7a0a4e77557
Léa Buguinet <lea.buguinet@gmail.com>
2021-05-11 15:44:55 +0200

    ColorThief sur la page ShowPiece

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -19,39 +19,3 @@ export default class App extends React.Component {
   } 
 
 }
-
-      {/* <BrowserRouter> */}
-        {/* <div> */}
-          {/* <h2>Ma super app</h2> */}
-
-         {/*  <Switch> */}
-{/* 
-            <Route path='/' exact>
-              <Collection/>
-            </Route>
-            <Route path='/concept' >
-              <Concept/>
-            </Route>
-            <Route path='/basket' >
-              <Basket/>
-            </Route>
-            <Route path='/register' >
-              <Register/>
-            </Route>
-            <Route path='/UserAccount' >
-              <Register/>
-            </Route> */}
-            
-            {/*<Route path='/owner'>
-              <Owner/>
-            </Route>
-            <Route path='/pieces'>
-              <Pieces/>
-            </Route> */}
-
-          {/* </Switch> */}
-
-          {/* <Footer/>
-        </div>
-        </BrowserRouter> */}
- 
